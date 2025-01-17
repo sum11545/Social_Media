a370cac57fd1d44a9a81066fe286878bf3aef7d9
sum11545 <84441774+sum11545@users.noreply.github.com>
2025-01-17 12:32:34 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-mongoose.connect(process.env.MONGO_URI).then(()=>{
+mongoose.connect(process.env.MONGO_URi).then(()=>{
     console.log("connected to database")
 }).catch((err)=>{
     console.log(err)
